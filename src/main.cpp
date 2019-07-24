7cfa47928b25ab9a8d6f1c050815c1e7d45a0984
Eric Berquist <eric.berquist@gmail.com>
2019-07-23 23:01:15 -0400

    Benchmark C++ pair vector add

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <emmintrin.h>
 
+#include <benchmark/benchmark.h>
+
 void add_pair_manual(const std::array<double, 2> &v1,
                      const std::array<double, 2> &v2,
                      std::array<double, 2> &out) {
@@ -19,19 +21,44 @@ void add_pair_sse2(const std::array<double, 2> &v1,
     out[1] = sres[1];
 }
 
-int main() {
+// int main() {
+//     const std::array<double, 2> v1 { 1.0, 2.0 };
+//     const std::array<double, 2> v2 { 3.0, 4.0 };
+//     std::array<double, 2> res;
+
+//     res = {0.0, 0.0};
+//     std::cout << res[0] << " " << res[1] << std::endl;
+//     add_pair_manual(v1, v2, res);
+//     std::cout << res[0] << " " << res[1] << std::endl;
+//     res = {0.0, 0.0};
+//     std::cout << res[0] << " " << res[1] << std::endl;
+//     add_pair_sse2(v1, v2, res);
+//     std::cout << res[0] << " " << res[1] << std::endl;
+
+//     return 0;
+// }
+
+static void BM_add_pair_manual(benchmark::State &state) {
     const std::array<double, 2> v1 { 1.0, 2.0 };
     const std::array<double, 2> v2 { 3.0, 4.0 };
     std::array<double, 2> res;
-
-    res = {0.0, 0.0};
-    std::cout << res[0] << " " << res[1] << std::endl;
-    add_pair_manual(v1, v2, res);
-    std::cout << res[0] << " " << res[1] << std::endl;
-    res = {0.0, 0.0};
-    std::cout << res[0] << " " << res[1] << std::endl;
-    add_pair_sse2(v1, v2, res);
-    std::cout << res[0] << " " << res[1] << std::endl;
-
-    return 0;
+    for (auto _ : state) {
+        res = {0.0, 0.0};
+        add_pair_manual(v1, v2, res);
+    }
 }
+
+static void BM_add_pair_sse2(benchmark::State &state) {
+    const std::array<double, 2> v1 { 1.0, 2.0 };
+    const std::array<double, 2> v2 { 3.0, 4.0 };
+    std::array<double, 2> res;
+    for (auto _ : state) {
+        res = {0.0, 0.0};
+        add_pair_sse2(v1, v2, res);
+    }
+}
+
+BENCHMARK(BM_add_pair_manual);
+BENCHMARK(BM_add_pair_sse2);
+
+BENCHMARK_MAIN();
